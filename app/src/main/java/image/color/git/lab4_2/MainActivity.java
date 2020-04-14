11358000cbf2c5c78d5a73499e2232079a04e89d
DubstepKnight <nursmaster@gmail.com>
2020-04-14 16:28:59 +0300

    the data is successfully received

--- app/src/main/java/image/color/git/lab4_2/MainActivity.java
+++ app/src/main/java/image/color/git/lab4_2/MainActivity.java
@@ -3,6 +3,16 @@ package image.color.git.lab4_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +20,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "https://financialmodelingprep.com/api/v3/stock/real-time-price/GOOGL,AAPL,FB,NOK";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+//                        textView.setText("Response: " + response.toString());
+                        Log.d("Lab4", "" + response);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Log.d("Lab4", "" + error);
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+// Access the RequestQueue through your singleton class.
+//        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
-
-
-
 }
